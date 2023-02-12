c5b0d4646e54321ae0252d0fda68dad51eb0bfc0
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2023-02-12 13:40:19 -0300

    Função para erros e mudança em alguns nomes

--- src/terminal_functions.rs
+++ src/terminal_functions.rs
@@ -1,17 +1,31 @@
-use crossterm::{ terminal };
+
+// execute == lambda in python????
+use crossterm::{ terminal, execute };
+use errno::errno;
+use std::io::{ stdout };
 
 pub struct TerminalFunctions {
 
 }
 
 impl TerminalFunctions {
+  pub fn new() {
+    TerminalFunctions::start_raw_mode();
+  }
+
+  pub fn die<S: Into<String>>(message: S) { // Esteja sempre preparado ... para tudo!
+    TerminalFunctions::finish_raw_mode(); // Fecha o modo raw
+    eprintln!("{}: {}", message.into(), errno()); // Mostra o erro
+    std::process::exit(1) // Fecha o editor
+  }
 
   pub fn start_raw_mode() {
-    terminal::enable_raw_mode();
+    let _ = terminal::enable_raw_mode();
+    execute!(stdout(), terminal::EnterAlternateScreen).unwrap() // Função para abrir uma nova "tela" obrigado Ox
   }
 
   pub fn finish_raw_mode() {
-    terminal::disable_raw_mode();
+    let _  = terminal::disable_raw_mode();
   }
 
 }
